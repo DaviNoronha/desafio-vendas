c7c4f4dcd818f84d323f0b646252de8361c435fa
Davi Noronha <davinoronha.g@gmail.com>
2020-08-29 17:04:54 -0400

    Crud do Fabricante - Parte 1

---
+++ resources/views/fabricante/form.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+
+@section('title', 'Fabricantes')
+
+@section('content_header')
+    <h1>Formulário de Fabricante</h1>
+@stop
+
+@section('content')
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+
+@stop
