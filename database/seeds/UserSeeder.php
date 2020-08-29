22cceee35e34f92cde7489466b50e6062d11cc60
Davi Noronha <davinoronha.g@gmail.com>
2020-08-29 16:08:28 -0400

    Cadastro de usuário usando Seed

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+
+    public function run()
+    {
+        User::firstOrCreate(
+            [
+                'email'=>"admin@email.com"
+            ],
+            [
+                'name'=> 'Administrador',
+                'password'=> bcrypt('12345678')
+            ]
+        );
+    }
+}
